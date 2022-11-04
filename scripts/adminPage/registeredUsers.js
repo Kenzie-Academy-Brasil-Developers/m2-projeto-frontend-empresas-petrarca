20e8445cd1aee0bd8d04ec434159c6ab0659e21c
Petrarca <pedropetrarcar@gmail.com>
2022-11-04 02:37:56 -0300

    savepoint 5

--- scripts/adminPage/registeredUsers.js
+++ scripts/adminPage/registeredUsers.js
@@ -2,9 +2,45 @@ import { requestAllUsers } from "../requests/adminRequests/allUsers.js"
 
 const allUsersDisplay = document.querySelector('.registered-users-display')
 
-export async function renderAllUsers(){
-    const userList = await requestAllUsers()    
+export async function renderAllUsers() {
+    const userList = await requestAllUsers()
     userList.forEach(user => {
-        
+        if (user.is_admin == false) {
+            const card = document.createElement('div')
+            const userDataBox = document.createElement('div')
+            const depName = document.createElement('h5')
+            const depDescription = document.createElement('p')
+            const companyName = document.createElement('p')
+    
+            const options = document.createElement('div')
+
+            const edit = document.createElement('button')
+            const exclude = document.createElement('button')
+            const editImg = document.createElement('img')
+            const excludeImg = document.createElement('img')
+            
+            card.classList.add("user-display-card")
+            userDataBox.classList.add("department-description")
+            options.classList.add("department-options")
+    
+            depName.classList.add('username-display')
+
+            depName.innerText = user.username
+            depDescription.innerText = user.professional_level
+            companyName.innerText = user.department_uuid
+    
+            // console.log(user.department_uuid)
+            
+            editImg.src = "../../src/edit.svg"
+            excludeImg.src = "../../src/delete.svg"
+    
+            userDataBox.append(depName, depDescription, companyName)
+
+            edit.append(editImg)
+            exclude.append(excludeImg)
+            options.append(edit, exclude)
+            card.append(userDataBox, options)
+            allUsersDisplay.append(card)
+        }
     });
 }
--- scripts/userPage/modals/editUser.js
+++ scripts/userPage/modals/editUser.js
@@ -5,12 +5,18 @@ const bodyOrigin = document.querySelector('body')
 export function modalEditUser() {
     const background = document.createElement('div')
     const modalBox = document.createElement('div')
+    const header = document.createElement('div')
     const title = document.createElement('h4')
     const closeBtn = document.createElement('button')
     const nameInput = document.createElement('input')
     const emailInput = document.createElement('input')
     const passwordInput = document.createElement('input')
     const submitEdit = document.createElement('button')
+
+    background.classList.add('modal-background')
+    modalBox.classList.add('edit-user-modal')
+    header.classList.add('edit-user-modal-header')
+    submitEdit.classList.add('edit-user-submit')
 
     title.innerText = "Editar Perfil"
     closeBtn.innerText = "X"
@@ -20,7 +26,8 @@ export function modalEditUser() {
     emailInput.placeholder = "Seu e-mail"
     passwordInput.placeholder = "Sua senha"
 
-    modalBox.append(title, closeBtn, nameInput, emailInput, passwordInput,submitEdit)
+    header.append(title, closeBtn)
+    modalBox.append(header, nameInput, emailInput, passwordInput,submitEdit)
     background.append(modalBox)
     bodyOrigin.append(background)
 
