cfd6181926d582668ce46ab7a299f81658cd7c7b
Petrarca <pedropetrarcar@gmail.com>
2022-11-07 08:13:45 -0300

    console error hotfix

--- pages/adminPage/admin.js
+++ pages/adminPage/admin.js
@@ -3,7 +3,6 @@ import { currentLoginVerify } from "../../scripts/pageProtection/loginCheck.js"
 import { renderAllUsers } from "../../scripts/adminPage/registeredUsers.js"
 import { logoutFunc } from "../../scripts/adminPage/buttons/logout.js"
 import { createDepartment } from "../../scripts/adminPage/buttons/createDepartment.js"
-import { insertCoWorkers } from "../../scripts/userPage/coWorkers.js"
 
 window.onload = () => {
     currentLoginVerify()
@@ -13,4 +12,3 @@ logoutFunc()
 selectEnterpriseAdm()
 renderAllUsers()
 createDepartment()
-insertCoWorkers()
--- scripts/login/redirect.js
+++ scripts/login/redirect.js
@@ -4,9 +4,7 @@ export async function userRedirect(){
     let token = localStorage.getItem('token')
     if(token != null){
         let userIsAdm = await verifyUserType()
-        console.log(userIsAdm)
         if(userIsAdm.is_admin == true){
-            
             window.location.replace('../adminPage/admin.html')
         } else if(userIsAdm.is_admin == false) {
             window.location.replace('../userPage/user.html')
